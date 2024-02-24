5633f9a6b99f864b83236e9da8785478b2e7de41
ahmedeceng87 <ahmedeceng87@gmail.com>
2024-02-24 12:02:31 +0530

    Referenced Microsoft.Extensions.DependencyInjection nuget package and launched main window through dependency injection
    Referenced Microsoft.Extensions.DependencyInjection nuget package and launched main window through dependency injection

--- DependencyInjectionInWPF/App.xaml.cs
+++ DependencyInjectionInWPF/App.xaml.cs
@@ -1,14 +1,25 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 namespace DependencyInjectionInWPF
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+{    
     public partial class App : Application
     {
-    }
+        private ServiceProvider serviceProvider;       
 
+        private void ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();           
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ConfigureServices();
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
 }
